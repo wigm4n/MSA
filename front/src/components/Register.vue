3fb458d798a089ce55da5d2dd4eb3215cccce94b
Mascantosh <nxibq@mail.ru>
2019-02-11 12:06:44 +0300

    Стабильная версия Отправляются данные на бэк Начата работа над оформлением Обновлён .gitignore TODO: Написать README.md

---
+++ front/src/components/Register.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="Register">
+    <h1>Введите данные о новом преподавателе</h1>
+    <h2>Логин и пароль будут отправлены преподавателю на почту</h2>
+    <input type="text" name="email" v-model="input.email" placeholder="Почтовый ящик"/>
+    <input type="text" name="name" v-model="input.name" placeholder="Имя"/>
+    <input type="text" name="surname" v-model="input.surname" placeholder="Фамилия"/>
+    <input type="text" name="lastname" v-model="input.lastname" placeholder="Отчество"/>
+    <button type="button" v-on:click="sendData">Создать преподавателя</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  data () {
+    return {
+      input: {
+        email: '',
+        name: '',
+        surname: '',
+        lastname: ''
+      }
+    }
+  },
+  methods: {
+    sendData () {
+      return {
+        registerInput: {
+          email: this.input.email,
+          name: this.input.name,
+          surname: this.input.surname,
+          lastname: this.input.lastname
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #Register {
+    width: 500px;
+    border: 1px solid #CCCCCC;
+    background-color: #FFFFFF;
+    margin: auto;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
---
+++ front/webpack.config.js
@@ -0,0 +1,88 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },  
+  plugins: [
+         new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default'],
+        // In case you imported plugins individually, you must also require them here:
+        Util: "exports-loader?Util!bootstrap/js/dist/util",
+        Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+        
+      })
+  ],
+
+ resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
